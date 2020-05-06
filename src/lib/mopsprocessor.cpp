75e5cb83b9abb7fde5c8dadde8f05295e5c02e25
Álvaro Cebrián Juan <acebrianjuan@gmail.com>
2020-05-06 16:05:41 +0200

    Initialize fixed Data Item lists only once

--- src/lib/mopsprocessor.cpp
+++ src/lib/mopsprocessor.cpp
@@ -260,66 +260,84 @@ bool MopsProcessor::checkDataItemsList(const AsterixRecord &record,
 
 QVector<MopsProcessor::DataItemList> MopsProcessor::ed116TargetReportsMinimumFieldsCollection()
 {
-    QVector<DataItemList> minFields;
+    static QVector<DataItemList> minFields;
 
-    DataItemList mandatory;
-    mandatory.type = DataItemListType::Mandatory;
-    mandatory.items = QStringList() << QLatin1String("I000")   // Message Type
-                                    << QLatin1String("I010")   // Data Source Identifier
-                                    << QLatin1String("I020")   // Target Report Descriptor
-                                    << QLatin1String("I140")   // Time of Day
-                                    << QLatin1String("I270");  // Target Size & Orientation
-    //<< QLatin1String("I550");  // System Status
+    static bool initialized = false;
+    if (!initialized)
+    {
+        DataItemList mandatory;
+        mandatory.type = DataItemListType::Mandatory;
+        mandatory.items = QStringList() << QLatin1String("I000")   // Message Type
+                                        << QLatin1String("I010")   // Data Source Identifier
+                                        << QLatin1String("I020")   // Target Report Descriptor
+                                        << QLatin1String("I140")   // Time of Day
+                                        << QLatin1String("I270");  // Target Size & Orientation
+        //<< QLatin1String("I550");  // System Status
 
-    DataItemList disjunctive;
-    disjunctive.type = DataItemListType::Disjunctive;
-    disjunctive.items = QStringList() << QLatin1String("I040")   // Position in Polar Co-ordinates
-                                      << QLatin1String("I041")   // Position in WGS-84 Coordinates
-                                      << QLatin1String("I042");  // Position in Cartesian Coordinates
+        DataItemList disjunctive;
+        disjunctive.type = DataItemListType::Disjunctive;
+        disjunctive.items = QStringList() << QLatin1String("I040")   // Position in Polar Co-ordinates
+                                          << QLatin1String("I041")   // Position in WGS-84 Coordinates
+                                          << QLatin1String("I042");  // Position in Cartesian Coordinates
 
-    minFields << mandatory << disjunctive;
+        minFields << mandatory << disjunctive;
+        initialized = true;
+    }
+
     return minFields;
 }
 
 QVector<MopsProcessor::DataItemList> MopsProcessor::ed117TargetReportsMinimumFieldsCollection()
 {
-    QVector<DataItemList> minFields;
+    static QVector<DataItemList> minFields;
 
-    DataItemList mandatory;
-    mandatory.type = DataItemListType::Mandatory;
-    mandatory.items = QStringList() << QLatin1String("I000")   // Message Type
-                                    << QLatin1String("I010")   // Data Source Identifier
-                                    << QLatin1String("I020")   // Target Report Descriptor
-                                    << QLatin1String("I060")   // Mode 3/A Code in Octal
-                                    << QLatin1String("I091")   // Measured Height
-                                    << QLatin1String("I140")   // Time of Day
-                                    << QLatin1String("I161")   // Track Number
-                                    << QLatin1String("I170")   // Track Status
-                                    << QLatin1String("I220")   // Target Address
-                                    << QLatin1String("I500");  // Standard Deviation of Position
+    static bool initialized = false;
+    if (!initialized)
+    {
+        DataItemList mandatory;
+        mandatory.type = DataItemListType::Mandatory;
+        mandatory.items = QStringList() << QLatin1String("I000")   // Message Type
+                                        << QLatin1String("I010")   // Data Source Identifier
+                                        << QLatin1String("I020")   // Target Report Descriptor
+                                        << QLatin1String("I060")   // Mode 3/A Code in Octal
+                                        << QLatin1String("I091")   // Measured Height
+                                        << QLatin1String("I140")   // Time of Day
+                                        << QLatin1String("I161")   // Track Number
+                                        << QLatin1String("I170")   // Track Status
+                                        << QLatin1String("I220")   // Target Address
+                                        << QLatin1String("I500");  // Standard Deviation of Position
 
-    DataItemList disjunctive;
-    disjunctive.type = DataItemListType::Disjunctive;
-    disjunctive.items = QStringList() << QLatin1String("I040")   // Position in Polar Co-ordinates
-                                      << QLatin1String("I041")   // Position in WGS-84 Coordinates
-                                      << QLatin1String("I042");  // Position in Cartesian Coordinates
+        DataItemList disjunctive;
+        disjunctive.type = DataItemListType::Disjunctive;
+        disjunctive.items = QStringList() << QLatin1String("I040")   // Position in Polar Co-ordinates
+                                          << QLatin1String("I041")   // Position in WGS-84 Coordinates
+                                          << QLatin1String("I042");  // Position in Cartesian Coordinates
 
-    minFields << mandatory << disjunctive;
+        minFields << mandatory << disjunctive;
+        initialized = true;
+    }
+
     return minFields;
 }
 
 QVector<MopsProcessor::DataItemList> MopsProcessor::ed117ServiceMessagesMinimumFieldsCollection()
 {
-    QVector<DataItemList> minFields;
+    static QVector<DataItemList> minFields;
 
-    DataItemList mandatory;
-    mandatory.type = DataItemListType::Mandatory;
-    mandatory.items = QStringList() << QLatin1String("I000")   // Message Type
-                                    << QLatin1String("I010")   // Data Source Identifier
-                                    << QLatin1String("I140")   // Time of Day
-                                    << QLatin1String("I550");  // System Status
+    static bool initialized = false;
+    if (!initialized)
+    {
+        DataItemList mandatory;
+        mandatory.type = DataItemListType::Mandatory;
+        mandatory.items = QStringList() << QLatin1String("I000")   // Message Type
+                                        << QLatin1String("I010")   // Data Source Identifier
+                                        << QLatin1String("I140")   // Time of Day
+                                        << QLatin1String("I550");  // System Status
 
-    minFields << mandatory;
+        minFields << mandatory;
+        initialized = true;
+    }
+
     return minFields;
 }
 
