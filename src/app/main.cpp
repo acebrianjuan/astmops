63f3f51c35cf4478f4a625ec3f45e254f5b60168
Álvaro Cebrián Juan <acebrianjuan@gmail.com>
2020-06-18 13:02:21 +0200

    Add brief documentation and Copyright notice in file headers (app)

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,3 +1,22 @@
+/*!
+ * \file main.cpp
+ * \brief Entry point of the ASTMOPS program.
+ * \author Álvaro Cebrián Juan, 2020. acebrianjuan(at)gmail.com
+ *
+ * -----------------------------------------------------------------------
+ *
+ * Copyright (C) 2020 Álvaro Cebrián Juan <acebrianjuan@gmail.com>
+ *
+ * ASTMOPS is a surveillance performance analyzer
+ *      for Air Traffic Control
+ *
+ * This file is part of ASTMOPS.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * -----------------------------------------------------------------------
+ */
+
 //#include <QCoreApplication>
 #include <QDebug>
 
