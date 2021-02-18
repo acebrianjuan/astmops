6939ef359b114cede0eecec3488cdd5da1b45206
Álvaro Cebrián Juan <acebrianjuan@gmail.com>
2021-02-18 13:34:24 +0100

    Create AreaHash container

---
+++ src/lib/areahash.h
@@ -0,0 +1,84 @@
+/*!
+ * \file areahash.h
+ * \brief
+ * \author Álvaro Cebrián Juan, 2020. acebrianjuan(at)gmail.com
+ *
+ * -----------------------------------------------------------------------
+ *
+ * Copyright (C) 2020 Álvaro Cebrián Juan <acebrianjuan@gmail.com>
+ *
+ * ASTMOPS is a surveillance performance analyzer
+ *      for Air Traffic Control
+ *
+ * This file is part of ASTMOPS.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * -----------------------------------------------------------------------
+ */
+
+#ifndef ASTMOPS_AREAHASH_H
+#define ASTMOPS_AREAHASH_H
+
+#include "aerodrome.h"
+#include <QHash>
+
+struct NamedArea
+{
+    NamedArea() = default;
+
+    NamedArea(const Aerodrome::Area area)
+        : area_(area)
+    {
+    }
+
+    NamedArea(const Aerodrome::Area area, const QString& name)
+        : area_(area), name_(name)
+    {
+    }
+
+    Aerodrome::Area area_ = Aerodrome::Area::None;
+    QString name_;
+};
+Q_DECLARE_METATYPE(NamedArea);
+
+#if (QT_VERSION < QT_VERSION_CHECK(6, 0, 0))
+uint qHash(const NamedArea& narea, uint seed)
+#else
+size_t qHash(const NamedArea& narea, size_t seed)
+#endif
+{
+    QtPrivate::QHashCombine hash;
+    seed = hash(seed, narea.name_);
+    seed = hash(seed, narea.area_);
+    return seed;
+}
+
+bool operator==(const NamedArea& a, const NamedArea& b)
+{
+    return a.area_ == b.area_ &&
+           a.name_ == b.name_;
+}
+
+template <typename T>
+struct AreaHash : public QHash<NamedArea, T>
+{
+    QVector<typename AreaHash::iterator> findByArea(Aerodrome::Area wantedArea)
+    {
+        QVector<typename AreaHash::iterator> result;
+
+        for (auto it = this->begin(), last = this->end(); it != last; ++it)
+        {
+            const Aerodrome::Area itArea = it.key().area_;
+
+            if ((itArea | wantedArea) == wantedArea)
+            {
+                result.append(it);
+            }
+        }
+
+        return result;
+    }
+};
+
+#endif  // ASTMOPS_AREAHASH_H
---
+++ src/test/areahashtest/areahashtest.cpp
@@ -0,0 +1,144 @@
+/*!
+ * \file areahashtest.cpp
+ * \brief Implements unit tests for the AreaHash container.
+ * \author Álvaro Cebrián Juan, 2020. acebrianjuan(at)gmail.com
+ *
+ * -----------------------------------------------------------------------
+ *
+ * Copyright (C) 2020 Álvaro Cebrián Juan <acebrianjuan@gmail.com>
+ *
+ * ASTMOPS is a surveillance performance analyzer
+ *      for Air Traffic Control
+ *
+ * This file is part of ASTMOPS.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * -----------------------------------------------------------------------
+ */
+
+#include "areahash.h"
+#include <QObject>
+#include <QtTest>
+
+class AreaHashTest : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void test();
+};
+
+void AreaHashTest::test()
+{
+    AreaHash<int> areaHash;
+
+    NamedArea runway1 = NamedArea(Aerodrome::Area::Runway, QLatin1String("18/36"));
+    NamedArea runway2 = NamedArea(Aerodrome::Area::Runway, QLatin1String("09/27"));
+    NamedArea taxiway = NamedArea(Aerodrome::Area::Taxiway);
+    NamedArea apronLane = NamedArea(Aerodrome::Area::ApronLane);
+    NamedArea stand = NamedArea(Aerodrome::Area::Stand);
+    NamedArea airborne1 = NamedArea(Aerodrome::Area::Airborne1);
+    NamedArea airborne2 = NamedArea(Aerodrome::Area::Airborne2);
+
+    areaHash.insert(runway1, 100);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::All).size() == 1);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Airborne).size() == 0);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Airborne1).size() == 0);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Airborne2).size() == 0);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Ground).size() == 1);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Movement).size() == 1);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Manoeuvering).size() == 1);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Runway).size() == 1);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Taxiway).size() == 0);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Apron).size() == 0);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::ApronLane).size() == 0);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Stand).size() == 0);
+
+    areaHash.insert(runway2, 100);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::All).size() == 2);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Airborne).size() == 0);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Airborne1).size() == 0);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Airborne2).size() == 0);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Ground).size() == 2);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Movement).size() == 2);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Manoeuvering).size() == 2);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Runway).size() == 2);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Taxiway).size() == 0);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Apron).size() == 0);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::ApronLane).size() == 0);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Stand).size() == 0);
+
+    areaHash.insert(taxiway, 100);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::All).size() == 3);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Airborne).size() == 0);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Airborne1).size() == 0);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Airborne2).size() == 0);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Ground).size() == 3);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Movement).size() == 3);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Manoeuvering).size() == 3);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Runway).size() == 2);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Taxiway).size() == 1);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Apron).size() == 0);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::ApronLane).size() == 0);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Stand).size() == 0);
+
+    areaHash.insert(apronLane, 100);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::All).size() == 4);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Airborne).size() == 0);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Airborne1).size() == 0);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Airborne2).size() == 0);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Ground).size() == 4);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Movement).size() == 4);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Manoeuvering).size() == 3);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Runway).size() == 2);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Taxiway).size() == 1);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Apron).size() == 1);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::ApronLane).size() == 1);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Stand).size() == 0);
+
+    areaHash.insert(stand, 100);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::All).size() == 5);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Airborne).size() == 0);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Airborne1).size() == 0);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Airborne2).size() == 0);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Ground).size() == 5);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Movement).size() == 5);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Manoeuvering).size() == 3);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Runway).size() == 2);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Taxiway).size() == 1);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Apron).size() == 2);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::ApronLane).size() == 1);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Stand).size() == 1);
+
+    areaHash.insert(airborne1, 100);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::All).size() == 6);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Airborne).size() == 1);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Airborne1).size() == 1);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Airborne2).size() == 0);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Ground).size() == 5);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Movement).size() == 5);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Manoeuvering).size() == 3);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Runway).size() == 2);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Taxiway).size() == 1);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Apron).size() == 2);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::ApronLane).size() == 1);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Stand).size() == 1);
+
+    areaHash.insert(airborne2, 100);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::All).size() == 7);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Airborne).size() == 2);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Airborne1).size() == 1);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Airborne2).size() == 1);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Ground).size() == 5);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Movement).size() == 5);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Manoeuvering).size() == 3);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Runway).size() == 2);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Taxiway).size() == 1);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Apron).size() == 2);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::ApronLane).size() == 1);
+    QVERIFY(areaHash.findByArea(Aerodrome::Area::Stand).size() == 1);
+}
+
+QTEST_APPLESS_MAIN(AreaHashTest)
+#include "areahashtest.moc"
